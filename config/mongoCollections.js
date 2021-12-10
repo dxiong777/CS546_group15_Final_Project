20a8d809c3da4f9290b91db154bbb81e52f27c57
dxiong777 <95152183+dxiong777@users.noreply.github.com>
2021-12-10 11:16:25 -0500

    landing page merged with product search
    app
    config

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -1,29 +1,25 @@
-const dbConnection = require('./mongoConnection');
-
 /* This will allow you to have one reference to each collection per app */
 /* Feel free to copy and paste this this */
+const dbConnection = require('./mongoConnections');
 const getCollectionFn = (collection) => {
   let _col = undefined;
-
   return async () => {
     if (!_col) {
-      const db = await dbConnection();
+      const db = await dbConnection.getDb();
       _col = await db.collection(collection);
     }
-
     return _col;
   };
 };
-
-/* Now, you can list your collections here: */
 module.exports = {
-  shop: getCollectionFn('shop'),
   products: getCollectionFn('products'),
   user: getCollectionFn('user'),
   message: getCollectionFn('message'),
   comment: getCollectionFn('comment'),
   reviews: getCollectionFn('reviews'),
   replayMessages: getCollectionFn('replayMessages'),
+  shopkeeper: getCollectionFn('shopkeeper'),
   productsForSearch: getCollectionFn('productForSearch'),
   reviewForProduct: getCollectionFn('reviewForProduct')
 };
+
---
+++ config/mongoConnections.js
@@ -0,0 +1,30 @@
+const MongoClient = require('mongodb').MongoClient;
+const settings =  {
+    mongoConfig: {
+      serverUrl: 'mongodb://localhost:27017/',
+      database: 'Group_15'
+    }
+  };
+  const mongoConfig = settings.mongoConfig;
+  let _connection = undefined;
+let _db = undefined;
+
+
+module.exports = {
+    getDb : async () => {
+
+  if (!_connection) {
+    _connection = await MongoClient.connect(mongoConfig.serverUrl, {
+      useNewUrlParser: true
+    }, {useUnifiedTopology: true});
+    _db = await _connection.db(mongoConfig.database);
+  }
+
+  return _db;
+
+},
+removeConnection: async () => {
+    _connection.close();
+  }
+}
+
